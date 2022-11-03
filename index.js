64730660ac25b74d64283c8d3d7898796ac7b855
Hoang Tran <jashoang2583@gmail.com>
2022-11-03 10:06:45 -0700

    commit tree 5b08e1b1adfb18f1357322ae7cececdc3403ce7d

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("text")
+    text.textContent = "This is really cool!"
+    console.log("This is really cool!");
+});
